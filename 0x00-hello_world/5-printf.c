5b1412253646580485fba305dd2c441a77cce74d
Igah Franklin C <82556966+igah-franklin@users.noreply.github.com>
2022-07-04 22:53:19 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
 * main - Entry point
 *
-* Return: 0 if exited properly, non-zero otherwise
+* Return: always 0(success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
