6133fb68dcdad03074d0e781d745efead6f2018b
Igah Franklin C <82556966+igah-franklin@users.noreply.github.com>
2022-07-04 21:26:53 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
-*main-entry point,
-*
-*Return: 0
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
