6efbaa6c0bf50bc81a0541850c1e0ce13110bf3e
Igah Franklin C <82556966+igah-franklin@users.noreply.github.com>
2022-07-05 00:43:19 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -11,12 +14,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-       printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 if (n == 0)
-       printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 if (n < 0)
 {
-       printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 }
 return (0);
 }
