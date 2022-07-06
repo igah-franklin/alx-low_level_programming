5d37fd8d1676f006d5a4b2d4cfe66b0d4be63e01
Igah Franklin C <82556966+igah-franklin@users.noreply.github.com>
2022-07-06 05:11:44 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,10 @@
 /**
 * print_alphabet_x10 -> prints the lowercase alphabets
 */
-
 void print_alphabet_x10(void)
 {
 	int j;
 	int x;
-
 	for (x = 0; x < 10; x++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
